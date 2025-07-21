f93f5bb45c2900385d37f55f440e1848e1a9b803
Ray <rb@MacBook-Air.local>
2025-07-21 16:22:45 -0400

    first LeetCode solution

---
+++ 0x01_two_sum.py
@@ -0,0 +1,16 @@
+"""
+LeetCode
+
+TwoSum Solution
+"""
+
+# Returns the indices of two numbers (from a list of numbers labeled nums) that add up to a given integer target. 
+def two_sum(nums, target):
+    # Creates an inverted dictionary where the values are indices. This allows a solution of O(n) time.
+    nums_indexed = {value: index for index, value in enumerate(nums)}
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        # Ensures indices are unique, weeding out duplicate solutions.
+        if complement in nums_indexed and i != nums_indexed[complement]:
+            return [i, nums_indexed[complement]]
+    return [] # Explicitly returns an empty list when no solution is found.
